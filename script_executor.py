58eb62637da10fcb0fc88ff9938593d9680fc3e6
Asutosh Panda <panda.asutosh04@gmail.com>
2021-02-07 23:24:16 +0530

    Update script_executor.py

--- script_executor.py
+++ script_executor.py
@@ -1,6 +1,6 @@
 import subprocess
 
-git_url = "https://github.com/sunilkumarmohapatra/InstagramJetpackCompose.git"
+default_git_url = "https://github.com/Asutosh11/GroceryTracker.git"
 
 subprocess.call(['sh', 'android_sdk_tools_installer.sh']) 
-subprocess.call(['sh', 'apk_installer_from_git.sh', git_url]) 
+subprocess.call(['sh', 'apk_installer_from_git.sh', default_git_url]) 
