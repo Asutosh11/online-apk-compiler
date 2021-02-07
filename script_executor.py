53b9d8f8ac0d2a5eba988a86646f6458842fd4c6
Asutosh Panda <panda.asutosh04@gmail.com>
2021-02-08 00:10:43 +0530

    add html form and python form processing code - untested code

--- script_executor.py
+++ script_executor.py
@@ -1,6 +1,21 @@
 import subprocess
 
-git_url = "https://github.com/sunilkumarmohapatra/InstagramJetpackCompose.git"
 
-subprocess.call(['sh', 'android_sdk_tools_installer.sh']) 
-subprocess.call(['sh', 'apk_installer_from_git.sh', git_url]) 
+from flask import Flask, request, render_template  
+  
+# Flask constructor 
+app = Flask(__name__)    
+  
+# A decorator used to tell the application 
+# which URL is associated function 
+@app.route('/', methods =["GET", "POST"]) 
+def gfg(): 
+    if request.method == "POST": 
+        git_url = request.form.get("giturl") 
+        git_url = "https://github.com/sunilkumarmohapatra/InstagramJetpackCompose.git"
+        subprocess.call(['sh', 'android_sdk_tools_installer.sh']) 
+        subprocess.call(['sh', 'apk_installer_from_git.sh', git_url]) 
+    return render_template("input-form.html") 
+  
+if __name__=='__main__': 
+   app.run() 
