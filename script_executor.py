f91ec94a280c78d977b702a42847ce0bf7e1b4a9
Asutosh Panda <panda.asutosh04@gmail.com>
2021-02-18 19:26:30 +0530

    add HTML/CSS form form for user input

--- script_executor.py
+++ script_executor.py
@@ -4,14 +4,18 @@ from flask import Flask, request, render_template
 app = Flask(__name__)    
 
 git_url = "https://github.com/Asutosh11/GroceryTracker.git"
+
+@app.route('/')
+def render_index_page():
+   return render_template('index.html')
   
-@app.route('/', methods =["GET", "POST"]) 
-def gfg(): 
+@app.route('/result', methods =["POST"]) 
+def proess_html_input(): 
     if request.method == "POST": 
         git_url = request.form.get("giturl") 
         subprocess.call(['sh', 'android_sdk_tools_installer.sh']) 
         subprocess.call(['sh', 'apk_installer_from_git.sh', git_url]) 
-    return render_template("input-form.html") 
+    return render_template("process_status.html") 
   
 if __name__=='__main__': 
     app.run() 
