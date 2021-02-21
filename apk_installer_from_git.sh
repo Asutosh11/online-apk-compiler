8cebf6e7dc2c14b4cd4a57f22506a6017384f87b
Asutosh Panda <panda.asutosh04@gmail.com>
2021-02-22 00:11:13 +0530

    Update apk_installer_from_git.sh

--- apk_installer_from_git.sh
+++ apk_installer_from_git.sh
@@ -5,7 +5,6 @@ echo "y" | sudo /usr/lib/android-sdk/cmdline-tools/tools/bin/sdkmanager --instal
 
 echo "no" | sudo /usr/lib/android-sdk/cmdline-tools/tools/bin/avdmanager create avd -n nexus6p -k "system-images;android-29;google_apis_playstore;x86" -b x86 -c 2048M -f
 
-
 sudo chmod 777 /usr/lib/android-sdk
 mkdir -p ~/emulator
 cp -v /usr/lib/android-sdk/emulator/* ~/emulator
@@ -36,6 +35,7 @@ cd ~/cloned_android_project/app/build/outputs/apk/debug
 
 ~/emulator/emulator -avd nexus6p & adb wait-for-device && sleep 5 && ~/adb install app-debug.apk
 
+# IMP: remove this comment if you want to launch the App on an emulator. Also comment out above line if installation on physical device not needed.
 # sudo /usr/lib/android-sdk/platform-tools/adb -s nexus6p install app-debug.apk
 
 
